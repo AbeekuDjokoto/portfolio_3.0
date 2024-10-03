7626574ae93458135df019e2d7fdc53e405d4c55
Abeeku Djokoto <djokotoabeeku619@gmail.com>
2024-10-03 12:39:32 +0000

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,10 +43,8 @@ export default function RootLayout({
           content="Mypurplepay is a forward thinking digital cooperative savings and credit platform that is poised to revolutionize the financial landscape. We provide accessible and flexible financing solutions to individuals and businesses filling in the gap left by traditional lending institutions. Our service aims to facilitate and promote financial wellness among individuals communities by offering a convenient platform for collective savings and alternative lending."
         />
 
-        <meta
-          property="og:url"
-          content="https://abeeku-portfolio.vercel.app/"
-        />
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://purple-vest.enyata.com/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="MyPurplePay" />
         <meta
@@ -55,32 +53,32 @@ export default function RootLayout({
         />
         <meta
           property="og:image"
-          content="https://asset.cloudinary.com/dgos0voss/bc76d3cd22650bcf2b9725a408536b9c"
+          content="https://res.cloudinary.com/dgos0voss/image/upload/v1727957751/metaImage_nmanzt.png"
         />
         <meta
           name="keywords"
           content="Thrift Savings, Target Savings, Micro-credit, Loan, Short-term loans, Salary loans, Savings loans, Finance, Responsible Borrowing, Financial Security"
         />
 
+        {/* <!-- Twitter Meta Tags --> */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="abeeku-portfolio.vercel.app" />
+        <meta property="twitter:domain" content="purple-vest.enyata.com" />
         <meta
           property="twitter:url"
-          content="https://abeeku-portfolio.vercel.app"
+          content="https://purple-vest.enyata.com/"
         />
         <meta name="twitter:title" content="MyPurplePay" />
         <meta
           name="twitter:description"
           content="Mypurplepay is a forward thinking digital cooperative savings and credit platform that is poised to revolutionize the financial landscape. We provide accessible and flexible financing solutions to individuals and businesses filling in the gap left by traditional lending institutions. Our service aims to facilitate and promote financial wellness among individuals communities by offering a convenient platform for collective savings and alternative lending."
         />
-        <meta
-          name="twitter:image"
-          content="https://asset.cloudinary.com/dgos0voss/bc76d3cd22650bcf2b9725a408536b9c"
-        />
+        <meta name="twitter:image" content="/metaImage.png" />
         <meta
           name="twitter:keywords"
           content="Thrift Savings, Target Savings, Micro-credit, Loan, Short-term loans, Salary loans, Savings loans, Finance, Responsible Borrowing, Financial Security"
         />
+
+        {/* <!-- Meta Tags Generated via https://www.opengraph.xyz --> */}
       </head>
       <body className={inter.className}>
         {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
